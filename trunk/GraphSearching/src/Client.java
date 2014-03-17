caef710f95c7f0b1f31680c367b0a9d3c246d908
vajanko <vajanko@f301d3a7-55df-40d9-b6c8-b1abe4c251df>
2014-03-17 18:22:19 +0000

    Refactored code of Client to make it more readable. Added rmic to make script.

--- trunk/GraphSearching/src/Client.java
+++ trunk/GraphSearching/src/Client.java
@@ -10,113 +10,178 @@ public class Client {
 	private static final long SEED = 100;	// use the same graph for all methods, also find distances between the same nodes
 	
 	// How many nodes and how many edges to create.
-		private static final int GRAPH_NODES = 500;
-		private static int GRAPH_EDGES = 2000;
+	private static final int GRAPH_NODES = 500;
+	private static int GRAPH_EDGES = 2000;
+	
+	// How many searches to perform
+	private static final int SEARCHES = 1;
+	private static int attempt = 0;
+	private static int transDistance = 4;
+	private static String method;
+	
+	private static Node [] aNodes;
+	
+	private static Random oRandom = new Random();
+	private static Searcher oSearcher = null;
+	private static GraphGenerator oGenerator = null;
+	
+	/**
+	 * Creates nodes of a graph.
+	 * @param iHowMany
+	 */
+	public static void createNodes (int iHowMany)
+	{
+		aNodes = new Node [iHowMany];
 		
-		// How many searches to perform
-		private static final int SEARCHES = 10;
-		private static int attempt = 0;
-		private static int transDistance = 4;
-		private static String method;
-		
-		private static Node [] aNodes;
-		
-		private static Random oRandom = new Random();
-		private static Searcher oSearcher = null;
-		private static GraphGenerator oGenerator = null;
-		
-		/**
-		 * Creates nodes of a graph.
-		 * @param iHowMany
-		 */
-		public static void createNodes (int iHowMany)
+		for (int i = 0 ; i < iHowMany ; i ++)
 		{
-			aNodes = new Node [iHowMany];
-			
-			for (int i = 0 ; i < iHowMany ; i ++)
-			{
-				aNodes [i] = new NodeImpl ();
-			}
+			aNodes [i] = new NodeImpl ();
 		}
-		/**
-		 * Creates nodes of a graph.
-		 * @param iHowMany
-		 * @throws RemoteException 
-		 */
-		public static void createRemoteNodes(int iHowMany) throws RemoteException
+	}
+	/**
+	 * Creates nodes of a graph.
+	 * @param iHowMany
+	 * @throws RemoteException 
+	 */
+	public static void createRemoteNodes(int iHowMany) throws RemoteException
+	{
+		aNodes = new Node[iHowMany];
+		for (int i = 0; i < iHowMany; i++)
 		{
-			aNodes = new Node[iHowMany];
-			for (int i = 0; i < iHowMany; i++)
-			{
-				aNodes[i] = oGenerator.CreateNode();
-			}
+			aNodes[i] = oGenerator.CreateNode();
 		}
-		
-		/**
-		 * Creates a fully connected graph. 
-		 * @throws RemoteException 
-		 */
-		public static void connectAllNodes () throws RemoteException
+	}
+	
+	/**
+	 * Creates a fully connected graph. 
+	 * @throws RemoteException 
+	 */
+	public static void connectAllNodes () throws RemoteException
+	{
+		for (int iNodeFrom = 0 ; iNodeFrom < aNodes.length ; iNodeFrom++)
 		{
-			for (int iNodeFrom = 0 ; iNodeFrom < aNodes.length ; iNodeFrom++)
+			for (int iNodeTo = iNodeFrom + 1 ; iNodeTo < aNodes.length ; iNodeTo++)
 			{
-				for (int iNodeTo = iNodeFrom + 1 ; iNodeTo < aNodes.length ; iNodeTo++)
-				{
-					aNodes [iNodeFrom].addNeighbor (aNodes [iNodeTo]);
-					aNodes [iNodeTo].addNeighbor (aNodes [iNodeFrom]);
-				}
-			}
-		}
-		
-		/**
-		 * Creates a randomly connected graph.
-		 * @param iHowMany
-		 * @throws RemoteException 
-		 */
-		public static void connectSomeNodes (int iHowMany) throws RemoteException
-		{
-			for (int i = 0 ; i < iHowMany ; i ++)
-			{
-				int iNodeFrom = oRandom.nextInt (aNodes.length);
-				int iNodeTo = oRandom.nextInt (aNodes.length);
-				
 				aNodes [iNodeFrom].addNeighbor (aNodes [iNodeTo]);
+				aNodes [iNodeTo].addNeighbor (aNodes [iNodeFrom]);
 			}
 		}
-		
-		/**
-		 * Runs a quick measurement on the graph.
-		 * @param iHowMany
-		 * @throws RemoteException 
-		 */
-		public static void searchBenchmark (int iHowMany) throws RemoteException
+	}
+	
+	/**
+	 * Creates a randomly connected graph.
+	 * @param iHowMany
+	 * @throws RemoteException 
+	 */
+	public static void connectSomeNodes (int iHowMany) throws RemoteException
+	{
+		for (int i = 0 ; i < iHowMany ; i ++)
 		{
-			// Display measurement header.
-			//System.out.printf ("%8s %8s %8s %8s %13s %13s\n", "Attempt", "Nodes", "Edges", "Distance", "Time", "TTime");
-			for (int i = 0 ; i < iHowMany ; i ++)
+			int iNodeFrom = oRandom.nextInt (aNodes.length);
+			int iNodeTo = oRandom.nextInt (aNodes.length);
+			
+			aNodes [iNodeFrom].addNeighbor (aNodes [iNodeTo]);
+		}
+	}
+	
+	/**
+	 * Runs a quick measurement on the graph.
+	 * @param iHowMany
+	 * @throws RemoteException 
+	 */
+	public static void searchBenchmark (int iHowMany) throws RemoteException
+	{
+		// Display measurement header.
+		//System.out.printf ("%8s %8s %8s %8s %13s %13s\n", "Attempt", "Nodes", "Edges", "Distance", "Time", "TTime");
+		for (int i = 0 ; i < iHowMany ; i ++)
+		{
+			++attempt;
+			// Select two random nodes.
+			int iNodeFrom = oRandom.nextInt (aNodes.length);
+			int iNodeTo = oRandom.nextInt (aNodes.length);
+			
+			// Calculate distance, timing the operation.
+			long iTime = System.nanoTime ();
+			
+			int iDistance = oSearcher.getDistance (aNodes [iNodeFrom], aNodes [iNodeTo]);
+			iTime = System.nanoTime () - iTime;
+			
+			long iTTime = System.nanoTime ();
+			int iTDistance = oSearcher.getTransitiveDistance (transDistance, aNodes [iNodeFrom], aNodes [iNodeTo]);
+			iTTime = System.nanoTime () - iTTime;
+			
+			if (iDistance != iTDistance)
 			{
-				// Select two random nodes.
-				int iNodeFrom = oRandom.nextInt (aNodes.length);
-				int iNodeTo = oRandom.nextInt (aNodes.length);
+				System.out.printf("Standard and transitive algorithms inconsistent (%d != %d)\n", iDistance, iTDistance);
+				System.out.printf("transDistance: %d\n", transDistance);
+				System.out.printf("from: %d, to: %d", iNodeFrom, iNodeTo);
+				System.exit(0);
 				
-				// Calculate distance, timing the operation.
-				long iTime = System.nanoTime ();
-				int iDistance = oSearcher.getDistance (aNodes [iNodeFrom], aNodes [iNodeTo]);
-				iTime = System.nanoTime () - iTime;
-				
-				long iTTime = System.nanoTime ();
-				int iTDistance = oSearcher.getTransitiveDistance (transDistance, aNodes [iNodeFrom], aNodes [iNodeTo]);
-				iTTime = System.nanoTime () - iTTime;
-				
-				if (iDistance != iTDistance)
-				{
-					System.out.printf("Standard and transitive algorithms inconsistent (%d != %d)\n", iDistance, iTDistance);
-				} else {
-					// Print the measurement result.
-					System.out.printf ("%8s %8d %8d %8d %8d %13d %13d\n", method, ++attempt, GRAPH_NODES, GRAPH_EDGES, iDistance, iTime / 1000, iTTime / 1000);
-				}
+			} else {
+				// Print the measurement result.
+				System.out.printf ("%8s %8d %8d %8d %8d %13d %13d\n", method, attempt, GRAPH_NODES, GRAPH_EDGES, iDistance, iTime / 1000, iTTime / 1000);
 			}
 		}
+	}
+
+	private static void runBenchmarks(boolean remoteNodes) throws RemoteException
+	{
+		// initialize test number counter
+		attempt = 0;
+		// always use the same graphs for all methods
+		oRandom = new Random(SEED);
+		
+		// Create a randomly connected graph and do a quick measurement.
+		for (int edges = GRAPH_NODES; edges < GRAPH_NODES * 20; edges += GRAPH_NODES)
+		{
+			GRAPH_EDGES = edges;
+			
+			if (remoteNodes)
+				createRemoteNodes(GRAPH_NODES);
+			else
+				createNodes (GRAPH_NODES);
+			
+			connectSomeNodes (GRAPH_EDGES);
+			searchBenchmark (SEARCHES);
+		}
+	}
+	private static void runLSLN() throws RemoteException
+	{
+		oSearcher = new LSearcherImpl ();
+		method = "LSLN";
+		
+		runBenchmarks(false);
+	}
+	private static void runRSLN(String hostname) throws MalformedURLException, RemoteException, NotBoundException
+	{
+		oSearcher = (Searcher) Naming.lookup (hostname + "/searcher");
+		method = "RSLN";
+		
+		runBenchmarks(false);
+	}
+	private static void runLSRN(String hostname) throws MalformedURLException, RemoteException, NotBoundException
+	{
+		oSearcher = new LSearcherImpl();
+		oGenerator = (GraphGenerator) Naming.lookup(hostname + "/generator");
+		method = "LSRN";
+		
+		runBenchmarks(true);
+	}
+	private static void runRSRN(String hostname) throws MalformedURLException, RemoteException, NotBoundException
+	{
+		oSearcher = (Searcher) Naming.lookup (hostname + "/searcher");
+		oGenerator = (GraphGenerator) Naming.lookup(hostname + "/generator");
+		method = "RSRN";
+		
+		runBenchmarks(true);
+	}
+	private static void runAll(String hostname) throws MalformedURLException, RemoteException, NotBoundException
+	{
+		runLSLN();
+		runRSLN(hostname);
+		runLSRN(hostname);
+		runRSRN(hostname);
+	}
 	
 	public static void main(String[] args) {
 		try
@@ -124,93 +189,24 @@ public class Client {
 			String hostname = "//localhost";
 			if (args.length == 1)
 				hostname = args[0];
-			String searcherHost = hostname + "/searcher";
-			String generatorHost = hostname + "/generator";
-			
-			System.out.printf ("%8s %8s %8s %8s %13s %13s\n", "Attempt", "Nodes", "Edges", "Distance", "Time", "TTime");
 			
 			for (int i = 2; i < 5; i++)
 			{
 				transDistance = i;
-				System.out.println("Distance: " + transDistance);
-			
-			/*// A)
-			oSearcher = new SearcherImpl ();
-			// each option gets the same graphs
-			oRandom = new Random(SEED);
-			attempt = 0;
-			method = "LSLN";
-			
-			// Create a randomly connected graph and do a quick measurement.
-			for (int edges = GRAPH_NODES; edges < GRAPH_NODES * 20; edges += GRAPH_NODES)
-			{
-				GRAPH_EDGES = edges;
 				
-				createNodes (GRAPH_NODES);
-				connectSomeNodes (GRAPH_EDGES);
-				searchBenchmark (SEARCHES);
-			}*/
-			
-			// B)
-			oSearcher = (Searcher) Naming.lookup (searcherHost);
-			// each option gets the same graphs
-			oRandom = new Random(SEED);
-			attempt = 0;
-			method = "RSLN";
-			
-			// Create a randomly connected graph and do a quick measurement.
-			for (int edges = GRAPH_NODES; edges < GRAPH_NODES * 20; edges += GRAPH_NODES)
-			{
-				GRAPH_EDGES = edges;
+				System.out.println("Transitive distance: " + transDistance);
+				System.out.printf ("%8s %8s %8s %8s %8s %13s %13s\n", "Method", "Attempt", "Nodes", "Edges", "Distance", "Time", "TTime");
 				
-				createNodes (GRAPH_NODES);
-				connectSomeNodes (GRAPH_EDGES);
-				searchBenchmark (SEARCHES);
+				runAll(hostname);
 			}
 			
-			// C)
-			oSearcher = new SearcherImpl();
-			oGenerator = (GraphGenerator) Naming.lookup(generatorHost);
-			// each option gets the same graphs
-			oRandom = new Random(SEED);
-			attempt = 0;
-			method = "LSRN";
 			
-			// Create a randomly connected graph and do a quick measurement.
-			for (int edges = GRAPH_NODES; edges < GRAPH_NODES * 20; edges += GRAPH_NODES)
-			{
-				GRAPH_EDGES = edges;
-				
-				createRemoteNodes (GRAPH_NODES);
-				connectSomeNodes (GRAPH_EDGES);
-				searchBenchmark (SEARCHES);
-			}
-			
-			// C)
-			oSearcher = (Searcher) Naming.lookup (searcherHost);
-			oGenerator = (GraphGenerator) Naming.lookup(generatorHost);
-			// each option gets the same graphs
-			oRandom = new Random(SEED);
-			attempt = 0;
-			method = "RSRN";
-			
-			// Create a randomly connected graph and do a quick measurement.
-			for (int edges = GRAPH_NODES; edges < GRAPH_NODES * 20; edges += GRAPH_NODES)
-			{
-				GRAPH_EDGES = edges;
-				
-				createRemoteNodes (GRAPH_NODES);		// create object from client on the server
-				connectSomeNodes (GRAPH_EDGES);
-				searchBenchmark (SEARCHES);
-			}
-			
-			}
 		}
 		catch (Exception e)
 		{ 
 			System.out.println ("Client Exception: " + e.getMessage ());
+			
 			e.printStackTrace ();
 		}
-
 	}
 }
