db0e08b25cae4bf30328e12b777d6b9a321dbb79
vajanko <vajanko@f301d3a7-55df-40d9-b6c8-b1abe4c251df>
2014-04-02 08:27:16 +0000

    Added PDF document with description. Rewritten index and constant table.

--- trunk/ConstDb/ConstDb/main.cpp
+++ trunk/ConstDb/ConstDb/main.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
+#include<map>
+#include "du2.hpp"
+
+using namespace std;
+
+template <typename T> struct A 
+{ 
+	static int n;
+	static int f() { static int n = 0; return ++n; } 
+}; 
+template <typename T> int A<T>::n;
 
 int main(int argc, char **argv)
 {
+	table<int> tab{ 1, 2 };
+	int x = 100;
+
+	int val = 10;
+	int *ptr = &val;
+	int &ref = *ptr;
+
+	ptr = &val;
+	//ref = *ptr;
+	ref = 30;
 
 
 	system("pause");
