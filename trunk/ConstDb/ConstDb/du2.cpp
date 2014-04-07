c13692fd2e7183eacc0b0f0dafd9e5ff01e0ed7f
vajanko <vajanko@f301d3a7-55df-40d9-b6c8-b1abe4c251df>
2014-04-07 15:55:44 +0000

    Finished constant database with examples.

--- trunk/ConstDb/ConstDb/du2.cpp
+++ trunk/ConstDb/ConstDb/du2.cpp
@@ -1,2 +1,186 @@
+#include<iostream>
+#include<string>
 #include "du2.hpp"
+
+using namespace std;
+
+// Example 1) (from slides)
+enum color { red, green, blue };
+struct db_colors : public const_db<db_colors, color, std::string>
+{
+	db_colors()
+	{
+		insert(color::red, "red");
+		insert(color::green, "green");
+		insert(color::blue, "blue");
+	}
+};
+std::string to_string(color c) 
+{ 
+	return std::get<1>(find<db_colors, 0>(c));
+}
+color from_string(const std::string &str)
+{ 
+	return std::get<0>(find<db_colors, 1>(str));
+}
+void example1()
+{
+	cout << "EXAMPLE 1" << endl;
+	cout << color::red << " == " << to_string(color::red) << endl;
+	cout << from_string("blue") << " == " << "blue" << endl;
+	cout << endl;
+}
+// End of example 1)
+
+// Example 2) (tree structure)
+struct node
+{
+	int id;
+	const node &parent;
+
+	bool operator<(const node &n) const { return id < n.id; }
+
+	node(int id) : id(id), parent(*this) { }
+	node(int id, const node &parent) : id(id), parent(parent) { }
+};
+struct db_tree : public const_db<db_tree, int, node>
+{
+	static const node &find_node(int key)
+	{
+		return std::get<1>(find<db_tree, 0>(key));
+	}
+	
+	db_tree()
+	{
+		insert(0, node(0));
+		insert(1, node(1, find_node(0)));
+		insert(2, node(2, find_node(0)));
+
+		insert(3, node(3, find_node(1)));
+	}
+};
+void example2()
+{
+	cout << "EXAMPLE 2" << endl;
+
+	int key = 3;
+
+	// traversing path from leaf to the root
+	while (key != 0)
+	{
+		const node &n = db_tree::find_node(key);
+		cout << key << endl;
+		key = n.parent.id;
+	}
+
+	cout << endl;
+}
+// End of example 2)
+
+// Example 3) (personal)
+// Id, Name, Email, Age
+struct db_personal : public const_db<db_personal, int, string, string, int>
+{
+	db_personal()
+	{
+		insert(1, "Bob", "bob@gmail.com", 21);
+		insert(2, "Alice", "alice@email.cz", 25);
+		insert(3, "John", "john@yahoo.com", 21);
+		insert(4, "Mary", "mary@me.com", 18);
+	}
+};
+void example3()
+{
+	cout << "EXAMPLE 3" << endl;
+
+	vector<string> names{ "John", "Ashely", "Mary" };
+	for (auto &name : names)
+	{
+		try {
+			auto &row = find<db_personal, 1>(name);
+			cout << name << ": Id: " << get<0>(row) << " Email: " << get<2>(row) << endl;
+		}
+		catch (not_found_exception) {
+			cout << "There is no " << name << " in the database." << endl;
+		}
+	}
+	cout << endl;
+
+	vector<int> ages{ 25, 21, 19 };
+	for (auto age : ages)
+	{
+		cout << "Who is " << age << " years old?" << endl;
+
+		try {
+			auto &row = find<db_personal, 3>(age);
+			cout << "It is " << get<1>(row) << endl;
+		}
+		catch (not_found_exception) {
+			cout << "There is no one " << age << " years old or more of same age." << endl;
+		}
+	}
+
+	cout << endl;
+}
+// End of example 3)
+
+// Example 4) (permutation)
+struct permutation : public const_db<permutation, int, int>
+{
+	permutation()
+	{
+		// two cycles (0 - 3 - 2) and (1 - 5 - 4)
+		insert(0, 3);
+		insert(1, 5);
+		insert(2, 0);
+		insert(3, 2);
+		insert(4, 1);
+		insert(5, 4);
+	}
+};
+void fwd_permut(const std::vector<int> &src, std::vector<int> &trg)
+{
+	for (int i = 0; i < 6; ++i)
+	{
+		int idx = get<1>(find<permutation, 0>(i));
+		trg[idx] = src[i];
+	}
+}
+void print(const std::vector<int> &vec)
+{
+	for (int i : vec)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+}
+void example4()
+{
+	cout << "EXAMPLE 4" << endl;
+
+	std::vector<int> src{ 0, 1, 2, 3, 4, 5 };
+	std::vector<int> trg(6, 0);
+
+	cout << "Before permutation: ";
+	print(src);
+	for (int i = 0; i < 3; ++i)
+	{
+		fwd_permut(src, trg);
+		print(trg);
+		src = trg;
+	}
+
+	cout << endl;
+}
+// End of example 4)
+
+int main(int argc, char **argv)
+{
+	example1();
+	example2();
+	example3();
+	example4();
+
+	system("pause");
+}
 
--- trunk/ConstDb/ConstDb/du2.hpp
+++ trunk/ConstDb/ConstDb/du2.hpp
@@ -4,6 +4,7 @@
 #include<memory>
 #include<stdexcept>
 
+// Exception which is thrown when lookup in constant database is not successfull
 class not_found_exception : public std::runtime_error
 {
 public:
@@ -19,6 +20,8 @@ public:
 	typedef std::vector<RowType> data_type;		// shold be defined in the table
 	typedef typename data_type::iterator row_ptr;
 
+	// Gets iterator to row which contains "key" in column at "ColumnIndex" position
+	// or end() if such a row does not exist
 	row_ptr get(const column_type& key)
 	{ 
 		auto res = data_index_.find(key);
@@ -26,36 +29,48 @@ public:
 	}
 
 private:
+	// Index to data for column at "ColumnIndex" position
 	std::map<column_type, row_ptr> data_index_;
+	// Reference to raw data of the database
 	data_type &data_;
 
 public:
+	void insert(row_ptr it)
+	{	// new value inserted - update the index
+		auto key = std::get<ColumnIndex>(*it);
+		// Check whether current key is alredy present
+		if (data_index_.find(key) != data_index_.end())
+		{	// if key is present more than once row won't be accesible through this key
+			data_index_[key] = data_.end();
+		}
+		else
+		{
+			data_index_.insert(std::make_pair(key, it));
+		}
+	}
+
+	// Builds index on given database data (no const index is necessary)
 	index(data_type &data) : data_(data)
 	{	// build index
 		for (row_ptr it = data.begin(); it < data.end(); ++it)
 		{
-			auto key = std::get<ColumnIndex>(*it);
-			// Check whether current key is alredy present
-			if (data_index_.find(key) != data_index_.end())
-			{	// if key is present more than once row won't be accesible through this key
-				data_index_[key] = data.end();
-			}
-			else
-			{
-				data_index_.insert(std::make_pair(key, it));
-			}
+			insert(it);
 		}
 	}
 };
+// "index_holder" wrapps "index" class and creates it instance if necessary (lazy)
 // IndexType = type of index class, instance of upper template
 template<typename IndexType> class index_holder
 {
 public:
 	typedef typename IndexType::data_type data_type;
 private:
+	// Pointer to index if called from user code or nullptr otherwise
 	std::unique_ptr<IndexType> index_;
+	// Reference to raw data of the database - necessary when building index
 	data_type &data_;
 public:
+	// pretend to be index ...
 	IndexType *operator->()
 	{
 		// create index if does not exist yet
@@ -65,39 +80,59 @@ public:
 		// Seems to be very symbolic to me
 		return index_.operator->();
 	}
+	void insert(typename IndexType::row_ptr row)
+	{	// new value inserted - update the index (do not if index is not built yet)
+		if (index_ != nullptr)
+			index_->insert(row);
+	}
 
+	// Notice that index_ is uninitialized
 	index_holder(data_type &data) : data_(data), index_(nullptr) { }
 };
 
+// Just compile time collection of database indexes. The reason why we did not
+// use std::tuple is that each tuple argument type need its ColumnIndex 
 template<size_t ColumnIndex, typename RowType> struct index_tuple
 	: public index_tuple<ColumnIndex - 1, RowType>
 {
 	typedef index_tuple<ColumnIndex - 1, RowType> base_type;
-	typedef typename base_type::data_type data_type;
+	typedef typename base_type::data_type data_type;		// takes from inherited class
 
 	typedef index_holder<index<ColumnIndex, RowType>> index_type;
 	index_type index_holder_;
+
+	void insert(typename index<ColumnIndex, RowType>::row_ptr row)
+	{
+		index_holder_.insert(row);
+		base_type::index_holder_.insert(row);
+	}
 
 	index_tuple(data_type &data) : index_holder_(data), base_type(data) { }
 };
 template<typename RowType> struct index_tuple<0, RowType>
 {
 	typedef index_holder<index<0, RowType>> index_type;
-	typedef typename index_type::data_type data_type;
+	typedef typename index_type::data_type data_type;		// definition of database raw data type
 	
 	index_type index_holder_;
+
+	void insert(typename index<0, RowType>::row_ptr row)
+	{
+		index_holder_.insert(row);
+	}
 
 	index_tuple(data_type &data) : index_holder_(data) { }
 };
 
-//template<typename ...ColumnType> 
+// Table encapsulate raw data and indexes for each column
+// ValueType = std::tuple<...>
 template<typename ValueType> class table
 {
 public:
-	//typedef std::tuple<ColumnType...> value_type;
 	typedef ValueType value_type;
 	typedef std::vector<value_type> data_type;
 
+	// type of column data at ColIndex
 	template<size_t ColIndex>
 	using column_type = typename std::tuple_element<ColIndex, value_type>::type;
 
@@ -105,8 +140,12 @@ private:
 	data_type data_;
 	typedef typename data_type::iterator row_ptr;
 
+	// creates structure for as much indexes as number of columns
+	// -1 ??? => because column indexes are zero-based
 	index_tuple<std::tuple_size<value_type>::value - 1, value_type> index_tuple_;
 
+	// Lookup by column at "index" position - for internal purpose only
+	// Returns iterator to founded row or end() if not found
 	template<size_t index>
 	row_ptr get(const column_type<index> &key)
 	{
@@ -119,6 +158,9 @@ private:
 
 public:
 	
+	// Lookup by column at "index" position
+	// Returns reference to founded row or an exception is thrown when no such a row exist
+	// or multiple
 	template<size_t index>
 	const value_type &find(const column_type<index> &key)
 	{	
@@ -130,77 +172,57 @@ public:
 		return *row;
 	}
 
+	// Returns value indicating whether search by column at "index" will be successful
 	template<size_t index>
 	bool contains(const column_type<index> &key)
 	{
 		return get<index>(key) != data_.end();
 	}
 
-	explicit table(std::initializer_list<value_type> rows) :
-		data_(rows), index_tuple_(data_) { }
-	explicit table(const data_type &data) :
-		data_(data), index_tuple_(data_) { }
-};
-
-template<typename ...ColumnType>
-struct const_db
-{
-	typedef std::tuple<ColumnType...> value_type;
-	typedef typename table<value_type>::data_type data_type;
-
-	static typename table<value_type>::data_type data;
-
-	static void insert(ColumnType ...value)
+	// Inserts a new row to the table
+	void insert(const value_type &row)
 	{
-		data.push_back(std::make_tuple(value...));
+		data_.push_back(row);
+		row_ptr it = --data_.end();
+		index_tuple_.insert(it);
 	}
 
-	static table<value_type> get_table()
-	{
-		if ()
-		return table<value_type>(data);
-	}
+	table() : data_(), index_tuple_(data_) { }
+	explicit table(std::initializer_list<value_type> rows) : data_(rows), index_tuple_(data_) { }
+	explicit table(const data_type &data) : data_(data), index_tuple_(data_) { }
 };
 
-template<typename ...ColumnType>
-typename const_db<ColumnType...>::data_type const_db<ColumnType...>::data = typename const_db<ColumnType...>::data_type();
-
-template<typename DerivedType>
-class singleton
+// Singleton class for user defined constant database
+template<typename DbType, typename ...ColumnType>
+class const_db
 {
-	static DerivedType *instance_;
 public:
-	static void insert() { }
-	static DerivedType *instance()
-	{
-		if (instance_ == nullptr)
-			instance_ = new DerivedType();
+	typedef std::tuple<ColumnType...> value_type;
+	typedef table<value_type> table_type;
+private:
+	table_type table_;
 
-		return instance_;
+public:
+	// Returns reference to singleton table
+	static table_type &table()
+	{	// here user-defined constructor of DbType is called which allows to insert rows
+		static DbType instance_;
+
+		return instance_.table_;
 	}
-	singleton() { }
-};
-
-struct my_db : public singleton<my_db>
-	//const_db<int, bool, double>
-{
-	/*static table<value_type> table;
-	static void create()
+	// Inserts new row to constat database. Call this method from your constructor
+	void insert(ColumnType ...value)
 	{
-		insert(1, false, 1.0);
-	}*/
-	my_db()
-	{
-		insert();
-		insert();
+		table_.insert(std::make_tuple(value...));
 	}
 };
 
+// Search constant database of type ConstDb in columnt at "ColumnIndex" position and
+// return a reference to founded row or an exception is thrown when no such a row exist
+// or multiple with same value.
 template<typename ConstDb, size_t ColumnIndex>
 const typename ConstDb::value_type &
 	find(const typename std::tuple_element<ColumnIndex, typename ConstDb::value_type>::type &key)
 {
-	ConstDb::create();
-	auto table = ConstDb::get_table();
-	return table.find<ColumnIndex>(key);
+	return ConstDb::table().find<ColumnIndex>(key);
 }
