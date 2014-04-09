5b28f83b375923fccf9af7a0b808a8a97016f8c5
vajanko <vajanko@f301d3a7-55df-40d9-b6c8-b1abe4c251df>
2014-04-09 16:03:02 +0000

    Finished ocl_potencial exercise. Also an attempt to use sse instruction was made.

--- trunk/ocl_potential/ocl_potential/framework/implementation/implementation.hpp
+++ trunk/ocl_potential/ocl_potential/framework/implementation/implementation.hpp
@@ -17,20 +17,66 @@ class OCLProgram_Implementation : public OCLProgram
 private:
 	typedef float in_real_t;
 
-	std::vector<Edge> &mEdges;				///< Reference to the graph edges.
-	std::vector<length_t> &mLengths;		///< Reference to the graph lengths of the edges.
 	cl::Buffer mVelocities;
 	cl::Buffer mForces;
 	cl::Buffer mPoints;
 	index_t mPointCount;
+	cl::Buffer mEdges;
+	index_t mEdgeCount;
+
+	struct edge
+	{
+		unsigned int p1, p2, len;
+		edge() : p1(0), p2(0), len(0) { }
+	};
 
 public:
-	OCLProgram_Implementation() : mEdges(std::vector<Edge>()), mLengths(std::vector<length_t>())
+	OCLProgram_Implementation()
 	{
 		// This is the place to initialize mKernelSources member field.
 		// The path to the kernel file must be relative to the executable (i.e., to the main.cpp).
 		// i.e. mKernelSources["kernelName"] = KernelSource("implementation/kernelFile.cl", true);
+#ifndef __GNUC__
 		mKernelSources["iteration"] = KernelSource("C:\\Projects\\Homeworks\\ocl_potential\\ocl_potential\\framework\\implementation\\iteration.cl", true);
+#else
+		mKernelSources["iteration"] = KernelSource("implementation/iteration.cl", true);		
+#endif
+	}
+
+	length_t get_length(const std::vector<Edge>& edges, const std::vector<length_t> &lengths, size_t x, size_t y)
+	{
+		for (int i = 0; i < edges.size(); ++i)
+		{
+			if (edges[i].p1 == x && edges[i].p2 == y)
+				return lengths[i];
+		}
+		return 0;
+	}
+	edge *transform_graph(const std::vector<Edge>& edges, const std::vector<length_t> &lengths, size_t points, size_t &width)
+	{
+		std::vector<size_t> neighbours(edges.size(), 0);
+		for (size_t i = 0; i < edges.size(); ++i)
+		{
+			++neighbours[edges[i].p1];
+		}
+		size_t max_n = *std::max_element(neighbours.begin(), neighbours.end());
+
+		edge *graph = new edge[points * max_n];
+		for (size_t i = 0; i < points; ++i)
+		{
+			size_t k = 0;
+			for (size_t j = 0; j < points; ++j)
+			{
+				graph[i * max_n + j].len = get_length(edges, lengths, i, j);
+				if (graph[i * max_n + j].len != 0)
+				{
+					graph[i * max_n + j].p1 = i;
+					graph[i * max_n + j].p2 = j;
+				}
+			}
+		}
+
+		width = max_n;
 	}
 
 	// points = number of points in the simulation
@@ -40,30 +86,47 @@ public:
 		 * Initialization of the simulation.
 		 * You may allocate buffers, transform or precompute data, or transfer initial values to the GPU.
 		 */
-		//mEdges = edges;
-		//mLengths = lengths;
-
 		// allocate buffers
 		mPointCount = points;
+		mEdgeCount = edges.size();
 
 		Point *velocities = new Point[points];
 		for (size_t i = 0; i < points; ++i)
 			velocities[i].x = velocities[i].y = 0;
 
+		//size_t width;
+		//edge *graph = transform_graph(edges, lengths, points, width);
+
+		edge *my_edges = new edge[mEdgeCount];
+		for (size_t i = 0; i < mEdgeCount; ++i)
+		{
+			my_edges[i].p1 = edges[i].p1;
+			my_edges[i].p2 = edges[i].p2;
+			my_edges[i].len = lengths[i];
+		}
+
 		cl::Context context = *mContext;
 		mPoints = cl::Buffer(context, CL_MEM_READ_WRITE, sizeof(Point)* points);
-		mVelocities = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(Point)* points, velocities);
-		mForces = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(Point)* points, velocities);
+		mVelocities = cl::Buffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(Point)* points, velocities);
+		mForces = cl::Buffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(Point)* points, velocities);
+		mEdges = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(edge)* mEdgeCount, my_edges);
 	
 		cl::Kernel &kernel = mKernels->at("iteration");
 		kernel.setArg(0, sizeof(cl::Buffer), &mPoints);
 		kernel.setArg(1, sizeof(cl::Buffer), &mVelocities);
 		kernel.setArg(2, sizeof(cl::Buffer), &mForces);
-		kernel.setArg(3, mTimeQuantum);
+		kernel.setArg(3, sizeof(cl::Buffer), &mEdges);
+		kernel.setArg(4, mEdgeCount);
+		kernel.setArg(5, mVertexRepulsion);
+		kernel.setArg(6, mVertexMass);
+		kernel.setArg(7, mEdgeCompulsion);
+		kernel.setArg(8, mSlowdown);
+		kernel.setArg(9, mTimeQuantum);
 
-		cl_int err;
-		const cl::Device &dev = mDevices->at(0);
-		cl::CommandQueue commandQueue(context, dev, 0, &err);
+		//cl_int err;
+		//const cl::Device &dev = mDevices->at(0);
+		//cl::CommandQueue commandQueue(context, dev, 0, &err);
+
 		//commandQueue.enqueueWriteBuffer(mVelocities, CL_TRUE, 0, sizeof(Point)* mPointCount, velocities);
 		//commandQueue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(points), cl::NDRange(256), NULL, NULL);
 
@@ -83,13 +146,14 @@ public:
 		cl::Context context = *mContext;
 		cl::CommandQueue commandQueue(context, dev, 0, &err);
 
-		commandQueue.enqueueWriteBuffer(mPoints, CL_TRUE, 0, sizeof(float)*mPointCount, points.data());
+		commandQueue.enqueueWriteBuffer(mPoints, CL_TRUE, 0, sizeof(Point)*mPointCount, points.data());
 
 		cl::Kernel &kernel = mKernels->at("iteration");
 		commandQueue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(mPointCount), cl::NDRange(256), NULL, NULL);
 		commandQueue.finish();
 
 		err = commandQueue.enqueueReadBuffer(mPoints, CL_TRUE, 0, sizeof(Point)*mPointCount, points.data());
+		commandQueue.finish();
 	}
 
 
