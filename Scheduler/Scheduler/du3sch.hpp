b85f8bcc2146491b23760a763a84884f3b77d5b5
Ondrej Kováč <info@vajanko.me>
2014-04-29 15:09:44 +0000

    Added basic structure of worker thread.

--- Scheduler/Scheduler/du3sch.hpp
+++ Scheduler/Scheduler/du3sch.hpp
@@ -1,52 +1,136 @@
+// DU3sch.hpp
+// Ondrej Kov·Ë NPRG051 2013/2014
+
 #pragma once
 
-#include<vector>
+#include <memory>
+#include <future>
+#include <list>
+#include <vector>
 
-// optional design
-
+enum class task_state
+{
+	waiting,
+	running,
+	ready
+};
 template<typename T, typename TASK>
-class core
+class task_info
 {
 private:
-	// TODO:
-	//TASK[] queue;
+	typedef std::unique_ptr<std::future<T>> future_ptr;
+
+	task_state state_;
+	TASK &task_;
+	T result_;
+	std::future<T> op_;
 
 public:
-	void add_task(TASK &&task, std::size_t index)
+	void run()
 	{
-
+		// TODO: check and run
+		state_ = task_state::running;
+		//op_ = std::async(task_);
 	}
-	T get_task_result(std::size_t index)
+	T get_result()
 	{
-		return T();
+		// TODO: check and run
+		if (state_ == task_state::waiting)
+			result_ = task_();		// synchronous run
+		else if (state_ == task_state::running)
+			result_ = op_.get();	// asynchronous run (just wait for the thread)
+			
+		// at this point task must be already ready
+		state_ = task_state::ready;
+		return result_;
 	}
 
-	core() { }
+	task_info(TASK &&task) : task_(task), state_(task_state::waiting), 
+		result_(), op_() {}
+};
+
+template<typename T, typename TASK>
+class worker
+{
+public:
+	typedef task_info<T, TASK> job_type;
+	typedef std::shared_ptr<job_type> job_ptr;
+private:
+	typedef std::unique_ptr<std::thread> thread_ptr;
+
+	thread_ptr thread_;		// thread on which current worker is running
+	bool exit_;				// true if worker should exit
+	
+	// this could be a class, but for simplification
+	std::list<job_ptr> jobs_;	// a list of jobs to be executed
+	std::mutex jobs_mutex_;		// protects the jobs_ queue
+
+	// thread safe
+	job_ptr pop()
+	{
+		job_ptr job;
+		{
+			std::unique_lock<std::mutex> lock(jobs_mutex_);
+
+			job = jobs_.front();
+			jobs_.pop_front();
+		}
+		return job;
+	}
+	// thread safe
+	void push(job_ptr job)
+	{
+		std::lock_guard<std::mutex> lock(jobs_mutex_);
+		jobs_.push_back(job);
+
+		//queuePending.notify_one();
+	}
+
+	void run()
+	{
+		while (!exit_)
+		{
+			// take a job from queue
+			job_ptr job = pop();
+
+			// execute the job
+			//job->operator()();
+			//(*job)();
+		}
+	}
+public:
+	void add_task()
+	{
+		
+	}
+
+	worker() : thread_(new std::thread(std::bind(&worker::run, this))),
+		exit_(false) { }
+
+	// no copying
+	worker &operator=(const worker &w) = delete;
+	worker(const worker &w) = delete;
+
 };
 
 template<typename T, typename TASK>
 class Scheduler
 {
 public:
-	typedef core<T, TASK> core_type;
-private:
-	std::size_t last_index_;
-	std::vector<core_type> cores_;
-	typedef typename std::vector<core_type>::const_iterator core_ptr;
+	typedef std::shared_ptr<task_info<T, TASK>> task_info_ptr;
+	typedef worker<T, TASK> worker_type;
+	typedef std::unique_ptr<worker_type> worker_ptr;
 
-	core_ptr get_task_core(std::size_t index)
-	{
-		return cores_.end();
-	}
+private:
+	std::vector<worker_ptr> workers_;
+	std::vector<task_info_ptr> task_infos_;
+
+	
 
 public:
 	std::size_t add_task(TASK &&task)
 	{
-		++last_index_;
-		// add task to a core
-		cores_.front().add_task(std::forward<TASK>(task), last_index_);
 
-		return last_index_;
 	}
 
 	bool is_task_ready(std::size_t index)
@@ -56,11 +140,15 @@ public:
 
 	T get_task_result(std::size_t index)
 	{
-		return (get_task_core(index))->get_task_result(index);
 	}
 
-	Scheduler(std::size_t core_count) : last_index_(0), cores_(core_count)
+	Scheduler(std::size_t core_count) //: workers_(core_count)
 	{
+		// another possibility
+		for (std::size_t i = 0; i < core_count; ++i)
+		{
+			workers_.emplace_back(worker_ptr(new worker_type()));
+		}
 	}
 
 	~Scheduler()
