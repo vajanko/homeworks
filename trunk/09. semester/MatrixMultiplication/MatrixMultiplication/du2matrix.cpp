9df877275a79746b4b4b5a4b4b7ff42234717e9f
vajanko <vajanko@f301d3a7-55df-40d9-b6c8-b1abe4c251df>
2014-11-10 10:30:04 +0000

    Just testing peaces of code for matrix multiplication.

--- trunk/09. semester/MatrixMultiplication/MatrixMultiplication/du2matrix.cpp
+++ trunk/09. semester/MatrixMultiplication/MatrixMultiplication/du2matrix.cpp
@@ -1,10 +1,6 @@
 #include "du2matrix.hpp"
 
-std::size_t matrix::byte_size( ) const
+std::size_t matrix::byte_size() const
 {
-  return 0;
-}
-
-matrix::matrix( std::size_t m, std::size_t n )
-{
+	return 0;
 }
