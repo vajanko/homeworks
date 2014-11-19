6e5e03fee1ca07f3c4923ee8c1de614068f5184f
Ondrej Kvoáč <vajanko@gmail.com>
2014-11-19 11:04:24 +0100

    Added hash map of current mutex used in the LockOrderProperty listener.

--- 09. semester/JPF/daisyfs/src/fse2006daisy/verify/LockOrderProperty.java
+++ 09. semester/JPF/daisyfs/src/fse2006daisy/verify/LockOrderProperty.java
@@ -1,10 +1,42 @@
 package fse2006daisy.verify;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import gov.nasa.jpf.vm.Instruction;
 import gov.nasa.jpf.vm.ThreadInfo;
 import gov.nasa.jpf.vm.VM;
 
 public class LockOrderProperty extends PropertyAdapterBase {
+	
+	enum State {
+		Locked,
+		Unlocked
+	}
+	// 
+	private Map<Long, State> mutexStates;
+	
+	private boolean try_acq_rel(long lockno, State nextState) {
+		if (!mutexStates.containsKey(lockno))
+			mutexStates.put(lockno, State.Unlocked);
+		
+		if (mutexStates.get(lockno) == nextState)
+			return false;
+		
+		mutexStates.put(lockno, nextState);
+		return true;
+	}
+	private boolean try_acq(long lockno) {
+		return try_acq_rel(lockno, State.Locked);
+	}
+	private boolean try_rel(long lockno) {
+		return try_acq_rel(lockno, State.Unlocked);
+	}
+	@Override
+	public void reset() {
+		super.reset();
+		mutexStates.clear();
+	}
 	
 	@Override
 	public void instructionExecuted(VM vm, ThreadInfo th, Instruction nIns, Instruction ins) {
@@ -16,6 +48,6 @@ public class LockOrderProperty extends PropertyAdapterBase {
 	}
 	
 	public LockOrderProperty() {
-		
+		this.mutexStates = new HashMap<Long, LockOrderProperty.State>();
 	}
 }
