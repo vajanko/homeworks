ce412914ffc386f5d3217a182f8241706f97502d
Ondrej Kováč <vajanko@gmail.com>
2014-11-23 12:36:23 +0100

    Added new project for CodeContracts homework with implemented task 1 - integer container.

---
+++ 09. semester/CodeContracts/CodeContracts/IntCollection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeContracts
+{
+    public class IntCollection
+    {
+        private List<int> data = new List<int>();
+
+        public int Size()
+        {
+            return data.Count;
+        }
+
+        public void Add(int val)
+        {
+            data.Add(val);
+        }
+        public int Get(int index)
+        {
+            return data[index];
+        }
+        public int GetHigher(int val)
+        {
+            if (data.All(i => i <= val))
+                return val;
+
+            return data.First(i => i > val);
+        }
+        public void Remove(int index)
+        {
+            data.RemoveAt(index);
+        }
+        public void RemoveAll(int val)
+        {
+            data.RemoveAll(i => i == val);
+        }
+        public void Sort()
+        {
+            data.Sort();
+        }
+        public void Clear()
+        {
+            data.Clear();
+        }
+        
+    }
+}
