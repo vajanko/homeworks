56f2d6f8c90d2aeb991281c3208873639aea3239
Ondrej Kováč <vajanko@gmail.com>
2014-11-26 23:14:00 +0100

    Added test scripts which runs mlc.exe on all test files.

---
+++ 09. semester/MlaskalCompiler/public-rw/SWI098/du3/test.bat
@@ -0,0 +1,14 @@
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test1.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2-1.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2-2.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2-3.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2-4.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2-5.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2-6.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2-7.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2-8.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2-9.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2-a.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test2.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test41.mls
+.\Debug\mlc.exe ..\..\..\public-ro\mlc\du3\tests\du3-test44.mls
