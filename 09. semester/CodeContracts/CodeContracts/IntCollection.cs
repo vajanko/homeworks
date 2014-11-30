0ec7fe7adeef7faa9d7aa09ed9615c083e2f49b7
Ondrej Kováč <vajanko@gmail.com>
2014-11-30 17:52:28 +0100

    Added more contracts to the collection methods.

--- 09. semester/CodeContracts/CodeContracts/IntCollection.cs
+++ 09. semester/CodeContracts/CodeContracts/IntCollection.cs
@@ -31,13 +31,17 @@ namespace CodeContracts
 
         public void Add(int val)
         {
+            // size increases by 1
             Contract.Ensures(Contract.OldValue(Size()) + 1 == Size());
+            // value is added at the end
+            Contract.Ensures(Get(Size()) == val);
 
             data.Add(val);
         }
         [Pure]
         public int Get(int index)
         {
+            // given index does not exceeds array bounds
             Contract.Requires(index >= 0 && index < Size(), "Index in bounds");
 
             return data[index];
@@ -45,26 +49,35 @@ namespace CodeContracts
         [Pure]
         public int GetHigher(int val)
         {
-            if (data.All(i => i <= val))
-                return val;
+            // there exists an item that is smaller than given value "val"
+            Contract.Requires(Contract.Exists(0, Size(), i => Get(i) < val));
+            // returned value is higher than "val"
+            Contract.Ensures(Contract.Result<int>() > val);
 
             return data.First(i => i > val);
         }
         public void Remove(int index)
         {
             // this condition also cover the case when collection is empty
+            // can not call Remove on empty collection
             Contract.Requires(index >= 0 && index < Size());
+            // size decreases by 1
             Contract.Ensures(Contract.OldValue(Size()) + 1 == Size());
 
             data.RemoveAt(index);
         }
         public void RemoveAll(int val)
         {
+            // size decreases by count of "val" items in the collection
+            Contract.Ensures(Contract.OldValue(Size()) - data.Count(v => v == val) == Size());
+
             data.RemoveAll(i => i == val);
         }
         public void Sort()
         {
-            Contract.Ensures( data.Where((i, v) => i < Size() - 1 && v < Get(i + 1)).Count() == 0 );
+            // all items are sorted properly
+            Contract.ForAll(0, Size() - 1, i => Get(i) <= Get(i + 1));
+            //Contract.Ensures( data.Where((i, v) => i < Size() - 1 && v < Get(i + 1)).Count() == 0 );
 
             data.Sort();
         }
