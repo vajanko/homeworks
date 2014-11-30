bdd1adfb00343f69cad2c013064830c7e824e87b
Ondrej Kováč <vajanko@gmail.com>
2014-11-30 18:27:59 +0100

    Added a bunch of test code.

---
+++ 09. semester/CodeContracts/CodeContracts/BigNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeContracts
+{
+    class BigNumber
+    {
+        private IntCollection data = new IntCollection();
+
+        public BigNumber(string number)
+        {
+
+        }
+    }
+}
--- 09. semester/CodeContracts/CodeContracts/Program.cs
+++ 09. semester/CodeContracts/CodeContracts/Program.cs
@@ -11,13 +11,60 @@ namespace CodeContracts
     {
         static void Main(string[] args)
         {
-            IntCollection col = new IntCollection();
+            Graph graph = new Graph();
 
-            Contract.Assert(col.Size() == 0);
-            col.Add(1);
+            var first = 
+                graph.Create(1).Connect(
+                    graph.Create(2).Connect(
+                        graph.Get(1)), 
+                    graph.Create(3).Connect(
+                        graph.Create(4),
+                        graph.Create(5).Connect(
+                            graph.Get(2)),
+                        graph.Create(6).Connect(
+                            graph.Get(1))
+                        )
+                    );
+
+            IntCollection stack = new IntCollection();
+            stack.Add(first.Id);
+
+            IntCollection searchPath = new IntCollection();
+
+            while (stack.Size() > 0)
+            {
+                int id = stack.Get(stack.Size() - 1);
+                searchPath.Add(id);
+
+                Node node = graph.Get(id);
+                foreach(Node n in node.Neighbors)
+                {
+                    if (searchPath.c)
+                }
+
+            }
+
+
+            //IntCollection col = new IntCollection();
+
+            //Contract.Assert(col.Size() == 0);
+
+            //col.Add(100);
+            //Contract.Assert(col.Size() == 1);
+            //Contract.Assert(col.Get(0) == 100);
+
+            //col.Add(200);
             //Contract.Assert(col.Size() == 2);
-            int val = col.Get(2);
-            int size = col.Size();
+            //Contract.Assert(col.Get(0) == 100);
+            //Contract.Assert(col.Get(1) == 100);
+
+            
+
+            //Contract.Assert(col.Size() == 0);
+            //col.Add(1);
+            ////Contract.Assert(col.Size() == 2);
+            //int val = col.Get(2);
+            //int size = col.Size();
         }
     }
 }
