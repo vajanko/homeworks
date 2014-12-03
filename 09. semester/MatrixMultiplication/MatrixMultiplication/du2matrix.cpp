beb18934828ca889bbd9e7bb3920118d5ec83bc2
Ondrej Kováč <vajanko@gmail.com>
2014-12-03 14:03:19 +0100

    Implemented matrix multiplication for chunk_1 and chunk_16. Tested and working (must be exact size).

--- 09. semester/MatrixMultiplication/MatrixMultiplication/du2matrix.cpp
+++ 09. semester/MatrixMultiplication/MatrixMultiplication/du2matrix.cpp
@@ -1,3 +1,124 @@
 #include "du2matrix.hpp"
 
-//void chunk_mul()
+template<typename T>
+void chunk_mul(T &a, T &bt, T &res) { };
+template<typename T>
+void matrix_mul(T *a, T *bt, T *res, std::size_t dim1, std::size_t dim2, std::size_t dim3) { };
+template<typename T>
+bool get_value(T *data, std::size_t cols, std::size_t i, std::size_t j) { return false; }
+template<typename T>
+void set_value(T *data, std::size_t cols, std::size_t i, std::size_t j, bool e) { }
+
+template<>
+void matrix_mul<chunk_1>(chunk_1 *a, chunk_1 *b, chunk_1 *res, std::size_t dim1, std::size_t dim2, std::size_t dim3)
+{
+	for (std::size_t i = 0; i < dim1; ++i)
+	{
+		for (std::size_t j = 0; j < dim2; ++j)
+		{
+			res[i * dim1 + j] = false;
+			for (std::size_t k = 0; k < dim3; ++k)
+				res[i * dim1 + j] |= a[i * dim1 + k] & b[k * dim2 + j];
+		}
+	}
+}
+template<>
+bool get_value<chunk_1>(chunk_1 *data, std::size_t cols, std::size_t i, std::size_t j) { return data[i * cols + j]; }
+template<>
+void set_value<chunk_1>(chunk_1 *data, std::size_t cols, std::size_t i, std::size_t j, bool e) { data[i * cols + j] = e; }
+
+
+template<>
+void chunk_mul<chunk_16>(chunk_16 &a, chunk_16 &b, chunk_16 &res)
+{
+	// prepare A
+	chunk_16 A = 0;
+	chunk_16 L = 0xf000;
+	chunk_16 R = 0x0000;
+	chunk_16 ch_bit = 0x0800;
+
+	for (short i = 0; i < 4; ++i)
+	{
+		A |= (a & L) << i;
+		A |= (a & R) >> (4 - i);
+
+		L = (L >> 4) ^ ch_bit;
+		R = (R >> 4) ^ ch_bit;
+		ch_bit >>= 5;
+	}
+
+	const char16_t F = 0x8888;
+
+	for (short i = 0; i < 4; ++i)
+	{
+		chunk_16 t1 = A & F;
+		t1 |= t1 >> 1;
+		t1 |= t1 >> 2;
+
+		res |= t1 & b;
+		b = _rotl16(b, 4);
+		A <<= 1;
+	}
+}
+template<>
+void matrix_mul<chunk_16>(chunk_16 *a, chunk_16 *b, chunk_16 *res, std::size_t dim1, std::size_t dim2, std::size_t dim3)
+{
+	dim1 >>= 2;
+	dim2 >>= 2;
+	dim3 >>= 2;
+	for (std::size_t i = 0; i < dim1; ++i)
+	{
+		for (std::size_t j = 0; j < dim2; ++j)
+		{
+			res[i * dim1 + j] = 0;
+			for (std::size_t k = 0; k < dim3; ++k)
+				chunk_mul(a[i * dim1 + k], b[k * dim2 + j], res[i * dim1 + j]);	
+		}
+	}
+}
+template<>
+bool get_value<chunk_16>(chunk_16 *data, std::size_t cols, std::size_t i, std::size_t j) 
+{
+	chunk_16 block = data[i / 4 * cols / 4 + j / 4];
+	chunk_16 res = block & (0x8000 >> (i % 4 * 4 + j % 4));
+	
+	return res != 0;
+}
+template<>
+void set_value<chunk_16>(chunk_16 *data, std::size_t cols, std::size_t i, std::size_t j, bool e) 
+{
+	chunk_16 block = data[i / 4 * cols / 4 + j / 4];
+	chunk_16 res = 0x8000 >> (i % 4 * 4 + j % 4);
+	if (!e)
+		data[i / 4 * cols / 4 + j / 4] &= ~res;
+	else
+		data[i / 4 * cols / 4 + j / 4] |= res;
+}
+
+
+std::size_t matrix::byte_size() const { return data_size * sizeof(chunk_t); }
+std::size_t matrix::vsize() const { return _vsize; }
+std::size_t matrix::hsize() const { return _vsize; }
+void matrix::set(std::size_t i, std::size_t j, bool e)
+{
+	set_value<chunk_t>(data, hsize(), i, j, e);
+}
+bool matrix::get(std::size_t i, std::size_t j) const
+{
+	return get_value<chunk_t>(data, hsize(), i, j);
+}
+void matrix::assign_mul(const matrix & a, const matrix & b)
+{
+	matrix_mul(a.data, b.data, data, a.hsize(), a.vsize(), b.vsize());
+}
+
+matrix::matrix(std::size_t m, std::size_t n) : 
+	_vsize(m), _hsize(n), data_size(m * n / sizeof(chunk_t)), data(new chunk_t[data_size])	// todo: FIX
+{
+
+}
+
+matrix::~matrix()
+{
+	delete data;
+}
--- 09. semester/MatrixMultiplication/MatrixMultiplication/du2matrix.hpp
+++ 09. semester/MatrixMultiplication/MatrixMultiplication/du2matrix.hpp
@@ -3,45 +3,56 @@
 
 #include <cstddef>
 #include <string>
-#include <xmmintrin.h>
+//#include <xmmintrin.h>
+#include <emmintrin.h>
+
+typedef bool chunk_1;
+typedef unsigned short chunk_16;
+typedef __int32 chunk_32;
+typedef __int64 chunk_64;
+
+// type used by the matrix
+typedef chunk_16 chunk_t;
+
+
+//#define CHUNK_MUL(out, a, b) (out |= a & b)
+//#define ASSIGN_ZERO(out) (out = 0)
+
+//__m128i sse_trans_slice(__m128i x)
+//{
+//	union { unsigned short s[8]; __m128i m; } u;
+//	int    i;
+//	for (i = 0; i < 8; ++i) {
+//		u.s[7 - i] = _mm_movemask_epi8(x);
+//		x = _mm_slli_epi64(x, 1);
+//	}
+//	return  u.m;
+//}
+
+
+
 
 class matrix {
 private:
+	bool trans = false;
+	std::size_t data_size;
+	chunk_t *data;
+	std::size_t _vsize;
+	std::size_t _hsize;
 
 public:
 	static std::string name() { return "matrix"; }
 
-	std::size_t byte_size() const
-	{
-		return 0;
-	}
+	matrix(std::size_t m, std::size_t n);
+	~matrix();
 
-	matrix( std::size_t m, std::size_t n)
-	{
-	}
+	std::size_t byte_size() const;
+	std::size_t vsize() const;
+	std::size_t hsize() const;
+	void set(std::size_t i, std::size_t j, bool e);
+	bool get(std::size_t i, std::size_t j) const;
 
-	std::size_t vsize() const
-	{
-		return 0;
-	}
-
-	std::size_t hsize() const
-	{
-		return 0;
-	}
-
-	void set( std::size_t i, std::size_t j, bool e)
-	{
-	}
-
-	bool get( std::size_t i, std::size_t j) const
-	{
-		return false;
-	}
-
-	void assign_mul( const matrix & a, const matrix & b)
-	{
-	}
+	void assign_mul(const matrix & a, const matrix & b);
 };
 
 #endif
