bde6be10857c563af76aff9a8574af5171295503
Ondrej Kováč <vajanko@gmail.com>
2015-01-18 11:07:41 +0100

    Fixed compare script.

--- 09. semester/MlaskalCompiler/public-rw/SWI098/du6/compare.ps1
+++ 09. semester/MlaskalCompiler/public-rw/SWI098/du6/compare.ps1
@@ -12,16 +12,19 @@ foreach ($sourceFile in [System.IO.Directory]::EnumerateFiles($dir, "*.mls"))
     $outFile1 = $sourceFile.Replace("mls", "out")
     $asmFile1 = $sourceFile.Replace("mls", "moa")
     $icmFile1 = $sourceFile.Replace("mls", "icmout")
+    $binFile1 = $sourceFile.Replace("mls", "mo")
 
     $outFile2 = [System.IO.Path]::GetFileName($outFile1)
     $binFile2 = [System.IO.Path]::GetFileName($binFile1)
     $asmFile2 = [System.IO.Path]::GetFileName($asmFile1)
     $icmFile2 = [System.IO.Path]::GetFileName($icmFile1)
 
+    $binFile2 = $debugDir + $binFile2
+
     # compile the program
     & $mlcFile $sourceFile $binFile2 "-S$asmFile2" > $outFile2
     # run the program
-    # & $vmFile "$debugDir$binFile2" stack > $icmFile2
+    & $vmFile $binFile2 stack > $icmFile2
 
     # $outFile2 = $debugDir + $outFile2;
     # $asmFile2 = $debugDir + $asmFile2;
